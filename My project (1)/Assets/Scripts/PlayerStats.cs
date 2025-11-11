3eb99640adb4a0c7a91db056fa47043043ff7dc7
pmoro333 <pmoroiss3@gmail.com>
2025-11-11 11:33:27 +0200

    created playerstats

---
+++ My project (1)/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+public int health = 3;
+public int lives = 3;
+
+private float flickerTime = 0f;
+public float flickerDuration = 0.1f;
+
+private SpriteRenderer sr;
+
+public bool isImmune = false;
+private float immunityTime = 0f;
+public float immunityDuration = 1.5f;
+
+void SpriteFlicker()
+{
+    if (flickerTime < flickerDuration)
+    {
+        flickerTime = flickerTime + Time.deltaTime;
+    }
+    else if (flickerTime >= flickerDuration)
+    {
+        sr.enabled = !(sr.enabled);
+        flickerTime = 0;
+    }
+}
+
+public void TakeDamage(int damage)
+{
+    if (isImmune == false)
+    {
+        health = health - damage;
+        if (health < 0)
+            health = 0;
+
+        if (lives > 0 && health == 0)
+        {
+            FindObjectOfType<LvlMNGR>().RespawnPlayer();
+            health = 3;
+            lives--;
+        }
+        else if (lives == 0 && health == 0)
+        {
+            Debug.Log("Gameover");
+            Destroy(this.gameObject);
+        }
+
+        Debug.Log("Player Health: " + health.ToString());
+        Debug.Log("Player Lives: " + lives.ToString());
+
+        isImmune = true;
+        immunityTime = 0f;
+    }
+}
+
+
+   
+   
+   
+    void Start()
+    {
+       sr = GetComponent<SpriteRenderer>();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (isImmune == true)
+       {
+        SpriteFlicker();
+        immunityTime = immunityTime + Time.deltaTime;
+        if(immunityTime >= immunityDuration)
+        {
+            isImmune = false;
+            sr.enabled = true;
+        }
+
+       }
+
+    }
+}
